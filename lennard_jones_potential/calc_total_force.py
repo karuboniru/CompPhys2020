8db881f93d6a4fd5acaf54f76b754093e406489c
Qiyu Yan <yanqiyu01@gmail.com>
2020-05-20 20:55:33 +0800

    process

--- lennard_jones_potential/calc_total_force.py
+++ lennard_jones_potential/calc_total_force.py
@@ -1,28 +1,29 @@
 import numpy as np
 
 
-def generate_indices(n, i):
-    left = [i for k in range(n-1)]
-    right = [(k < i)*k + (k >= i)*(k+1) for k in range(n-1)]
-    return left, right
-
-
 def calc_force(r, r_c, mode, size, sigma, epsilon):
     if mode == 'periodic':
         np.add(r, size/2, out=r)
         np.mod(r, size, out=r)
         np.add(r, -size/2, out=r)
     nr = np.linalg.norm(r)
-    if(nr >= r_c):
-        return np.array(np.zeros_like(r))
-    return 4*epsilon*(12*sigma**12/nr**14 - 6*sigma**6/nr**8)*r
+    # if(nr >= r_c):
+    #     return np.array(np.zeros_like(r))
+    return 4*epsilon*(12*sigma**12/nr**14 - 6*sigma**6/nr**8)*r*(nr <= r_c)
 
 
 def calc_total_force(x, mode, size=None, sigma=1.0, epsilon=1.0, r_c=2.5):
-    n, _ = np.shape(x)
+    n, _ = x.shape
+    left, right = np.triu_indices(n, 1)
     force = np.empty_like(x)
-    for i in range(n):
-        left, right = generate_indices(n, i)
-        force[i] += np.sum(calc_force(r=x[left] - x[right], r_c=r_c, mode=mode,
-                                      size=size, sigma=sigma, epsilon=epsilon), axis=1)
+    dr = x[left] - x[right]
+    forcelist = calc_force(dr, r_c, mode, size, sigma, epsilon)
+    # following will not work, strange design of numpy
+    # force[right] -= forcelist
+    # force[left] += forcelist
+    for i in range(len(forcelist)):
+        force[left[i]] += forcelist[i]
+        force[right[i]] -= forcelist[i]
     return force
+
+print(calc_total_force(np.array([[0,0], [0.8,0.8], [-0.8,-0.8]], dtype=float),mode='hard', r_c=10))
--- molecular_dynamics/molecular_dynamics_system.py
+++ molecular_dynamics/molecular_dynamics_system.py
@@ -14,15 +14,31 @@ class molecular_dynamics_system(physics_system):
         self.time_step = time_step
         # assign velocities
         self.do_velocities_assign()
+        if mode == 'hard':
+            self.boundary = self.hard_boundary
+        elif mode == 'periodic':
+            self.boundary = self.periodic_boundary
+        else:
+            raise(ValueError)
 
     def do_velocities_assign(self):
         for i in range(self.count):
             for j in range(self.dimension):
                 self.velocity[i][j] = random() - 0.5
-        sumv = np.average(self.velocity, axis=1)
+        sumv = np.average(self.velocity, axis=0)
         sumv2 = np.sum(self.velocity**2)/self.count
         fs = np.sqrt(self.dimension*self.temp/sumv2)
         for i in range(self.count):
             self.velocity[i] -= sumv
             self.velocity[i] *= fs
             self.particles[i] -= self.velocity[i]*self.time_step
+
+    def periodic_boundary(self):
+        self.particles %= self.size
+
+    def hard_boundary(self):
+        if_out = ((self.particles % self.size - self.particles) != 0) * \
+            (-1)+((self.particles % self.size - self.particles) == 0)*1
+        self.particles = (self.particles*if_out) % self.size
+        if (self.velocity is not None):
+            self.velocity *= if_out
